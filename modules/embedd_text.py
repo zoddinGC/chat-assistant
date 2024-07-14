fb4b6a7118b69978d148998757b3d1d314ee3e0a
Gian Campos <zoddinGC@gmail.com>
2024-07-13 21:02:07 -0300

    replace embedding_in_chunks() function to work with Langchain LLM. Removed 2 old funcitons to embed text because the Langchain can automatically handles with the OpenAI Embedding method

--- modules/embedd_text.py
+++ modules/embedd_text.py
@@ -3,33 +3,10 @@ import numpy as np
 from transformers import AutoTokenizer, AutoModel
 from uuid import uuid4
 
+from langchain.docstore.document import Document
+
 # Local imports
 from modules.tokenize_text import create_text_splitter
-
-def embed_text(text: str) -> str:
-    # String for model AI in hugginface.co/models
-    model_id = 'mrm8488/bert-base-portuguese-cased-finetuned-squad-v1-pt'
-
-    # Load pre-trained model and tokenizer
-    embedder = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModel.from_pretrained(model_id)
-
-    # Encode text
-    inputs = embedder(text, return_tensors="pt")
-    outputs = model(**inputs)
-
-    # Extract embeddings
-    return outputs.last_hidden_state[0].detach().numpy()
-
-def pad_embedding(embedding: np.array, pad_dimensions: tuple[int, int]):
-    max_length, max_dim = pad_dimensions
-
-    if embedding.shape[0] > max_length:
-        embedding = embedding[:max_length]
-
-    padded_embedding = np.pad(embedding, ((0, max_length - embedding.shape[0]), (0, max_dim - embedding.shape[1])), 'constant')
-
-    return padded_embedding.flatten()
 
 def embedding_in_chunks(data: dict) -> tuple[list, np.array, tuple[int, int]]:
     # Create chunks list to return
@@ -40,30 +17,19 @@ def embedding_in_chunks(data: dict) -> tuple[list, np.array, tuple[int, int]]:
 
     # Split the text and append in chunk
     for txt, type in data.items():
+        # Split text based on tokens
         texts = text_splitter.split_text(txt)
-        chunks.extend([{
-            'id': str(uuid4()),
-            'text': texts[i],
-            'embedding': embed_text(texts[i]),
-            'type': type,
-            'chunk': i + len(chunks)
-        } for i in range(len(texts))])
 
-    # Find the max length and max embedding dimension
-    max_length = max(item['embedding'].shape[0] for item in chunks)
-    max_dim = max(item['embedding'].shape[1] for item in chunks)
-    pad_dimensions = (max_length, max_dim)
+        chunks.extend([Document(
+            page_content=texts[i],
+            metadata={
+                'id': str(uuid4()),
+                'type': type,
+                'chunk': i + len(chunks)
+            }
+        ) for i in range(len(texts))])
 
-    # Now retrieve the embed data for Faiss indexing method
-    new_data = []
-    for i, chunk in enumerate(chunks):
-        # Get padded embed chunk
-        pad_chunk = pad_embedding(chunk['embedding'], pad_dimensions)
-
-        new_data.append(pad_chunk)
-        chunks[i]['embedding'] = pad_chunk
-
-    return (chunks, np.array(new_data), pad_dimensions)
+    return chunks
 
 if __name__ == '__main__':
     embedding_in_chunks()
