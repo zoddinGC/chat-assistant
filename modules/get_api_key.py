acb9c36f11d5b6ac70ddc317346dba9be3892be7
Gian Campos <zoddinGC@gmail.com>
2024-07-16 10:12:40 -0300

    add function to write OpenAI API key to .env

--- modules/get_api_key.py
+++ modules/get_api_key.py
@@ -11,5 +11,17 @@ def get_API() -> str:
     """
     return os.getenv('OPENAI_API_KEY')
 
+def write_API(api_key: str):
+    """
+        Write the API key in .env
+
+        :param api_key: The API KEY to write in .env
+    """
+    # Create and open the .env file in write mode
+    with open('.env', 'w') as file:
+        # Write the content to the file
+        file.write(api_key)
+
+
 if __name__ == '__main__':
     get_API()
